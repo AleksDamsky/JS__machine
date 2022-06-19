3ec3dd746f166c7e96874e49d3b6ecb36be8d9af
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2022-06-19 12:14:21 +0300

    continue my personality ToDo

--- multiDop/JSscript/script.js
+++ multiDop/JSscript/script.js
@@ -1,2 +1,42 @@
 let addButton = document.querySelector('.Button');
 let addInput = document.querySelector('.Input');
+let addList = document.querySelector('.cases')
+
+let newArr = [];
+
+if(localStorage.getItem('todo')){
+    newArr = JSON.parse(localStorage.getItem('todo'))
+    displayList();
+};
+
+addButton.addEventListener('click', () =>{
+
+    let newObj = {
+        massage : addInput.value,
+        checked : false,
+    }
+
+    newArr.push(newObj);
+
+    displayList();
+
+    localStorage.setItem('todo', JSON.stringify(newArr));
+})
+
+
+function displayList(){
+
+    let displayCases = '';
+
+    newArr.forEach(function(item, i){
+        displayCases += `
+        <li>
+        <input type="checkbox" id="item_${i}" ${item.checked ? 'checked' : ''}>
+        <label for="item_${i}">${item.massage}</label>
+        </li>
+        `;
+
+        addList.innerHTML = displayCases;
+    })
+
+}
