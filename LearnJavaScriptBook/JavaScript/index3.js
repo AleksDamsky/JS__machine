4e7fa4f6f8ce0ecc9c112969414700c1af1d7aba
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2022-07-01 09:51:32 +0300

    first js ligic

--- LearnJavaScriptBook/JavaScript/index3.js
+++ LearnJavaScriptBook/JavaScript/index3.js
@@ -14,7 +14,7 @@ let hits = 0;
 
 //append variables fon number attempt
 
-let qusses = 0;
+let quesses = 0;
 
 //this variable which containe info about ship
 
@@ -23,9 +23,31 @@ let isSunk = false;
 while(isSunk == false){
     quess = prompt('Введите число от 0 до 6');
 
-    if (quess < 0 || quess > 0){
+    if (quess < 0 || quess > 6){
         alert('Ты вводишь не верные числа');
     }else{
-        qusses+=1;
+        quesses += 1;
+
+        if (quess == location1 || quess == location2 || quess == location3){
+            hits += 1;
+            alert(`ты попал в корабль!`)
+
+            if (hits == 3){
+
+                isSunk = true;
+
+                alert('Корабль потоплен');
+            }
+        }else{
+            alert(`ты не попал!
+            Пробуй еще раз!`)
+        }
+    }
+
+    alert(`Корабль потоплен за: 
+    ${quesses} ходов`);
+
+    if (isSunk == true){
+        break;
     }
 }
